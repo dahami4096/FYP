423bb1e5997bd856788c37b32b9792001a7e8cbc
Dahami Nethsarani <dahaminethsarani2000@gmail.com>
2025-10-31 17:02:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,14 @@
 import streamlit as st
-
+hide_streamlit_style = """
+    <style>
+    /* Hide hamburger menu */
+    #MainMenu {visibility: hidden;}
+    /* Hide footer */
+    footer {visibility: hidden;}
+    /* Optional: hide header (title bar) */
+    header {visibility: hidden;}
+    </style>
+"""
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 # Immediately redirect to the login page
 st.switch_page("pages/0_Login.py")
